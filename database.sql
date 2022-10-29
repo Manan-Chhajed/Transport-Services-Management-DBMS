ab2f31ed497d961a7cfe157500be45b342304495
Manan <mananhemant288@gmail.com>
2022-10-29 13:25:36 +0530

    rectified errors

--- database.sql
+++ database.sql
@@ -7,13 +7,13 @@ CREATE TABLE Account(
 CREATE TABLE Customer(
     CustomerId int(10) NOT NULL,
     FirstName varchar(20),
+    MiddleName varchar(20),
     LastName varchar(20),
     DOB varchar(10),
     Email varchar(30),
     UserName varchar(20) NOT NULL,
     Password varchar(20) NOT NULL,
-    FOREIGN KEY (UserName) REFERENCES Account(UserName),
-    FOREIGN KEY (Password) REFERENCES Account(Password),
+    FOREIGN KEY (UserName, Password) REFERENCES Account(UserName, Password),
     PRIMARY KEY (CustomerId, UserName, Password)
 );
 
@@ -21,7 +21,7 @@ CREATE TABLE Contact_Customer(
     ContactNo int(20) NOT NULL,
     CustomerId int(10) NOT NULL,
     FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
-    PRIMARY KEY (CustomerId,ContactNo)
+    PRIMARY KEY (CustomerId, ContactNo)
 );
 
 CREATE TABLE Driver(
@@ -33,8 +33,7 @@ CREATE TABLE Driver(
     Email varchar(30),
     UserName varchar(20) NOT NULL,
     Password varchar(20) NOT NULL,
-    FOREIGN KEY (UserName) REFERENCES Account(UserName),
-    FOREIGN KEY (Password) REFERENCES Account(Password),
+    FOREIGN KEY (UserName, Password) REFERENCES Account(UserName, Password),
     PRIMARY KEY (DriverId)
 );
 
@@ -42,10 +41,45 @@ CREATE TABLE Contact_Driver(
     ContactNo int(20) NOT NULL,
     DriverId int(10) NOT NULL,
     FOREIGN KEY (DriverId) REFERENCES Driver(DriverId),
-    PRIMARY KEY (DriverId,ContactNo)
+    PRIMARY KEY (DriverId, ContactNo)
 );
 
+CREATE TABLE Routes(
+    Source varchar(30) NOT NULL,
+    Destination varchar(30) NOT NULL,
+    Distance int,
+    Fare int,
+    RouteNo int,
+    PRIMARY KEY (RouteNo)
+);
 
+CREATE TABLE Vehicle(
+    LicenseNo int NOT NULL,
+    Model varchar(20),
+    Type varchar(20),
+    LastService varchar(15),
+    PRIMARY KEY (LicenseNo)
+);
+
+CREATE TABLE Driver_Vehicle_relation(
+    DriverId int NOT NULL,
+    LicenseNo int NOT NULL,
+    FOREIGN KEY (DriverId) REFERENCES Driver(DriverId),
+    FOREIGN KEY (LicenseNo) REFERENCES Vehicle(LicenseNo),
+    PRIMARY KEY (DriverId, LicenseNo)
+);
+
+CREATE TABLE Journey(
+    CustomerId int,
+    DriverId int,
+    RouteNo int,
+    LicenseNo int,
+    FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
+    FOREIGN KEY (DriverId) REFERENCES Driver(DriverId),
+    FOREIGN KEY (RouteNo) REFERENCES Routes(RouteNo),
+    FOREIGN KEY (LicenseNo) REFERENCES Vehicle(LicenseNo),
+    PRIMARY KEY (CustomerId, DriverId, RouteNo, LicenseNo)
+);
 
 insert into Account
 values ('sarvesh_13', 'abcd');
@@ -62,32 +96,37 @@ values ('chaitya_16', 'mnop');
 insert into Account
 values ('tejas_16', 'qrst');
 
-insert into Customer
-values (34,'nikhil','babar','27 oct','nikhilbabar@gmail.com', 'sarvesh_13', 'abcd');
 
 insert into Customer
-values (50,'Sarvesh','Deshpande','19 sep','Sarvesh@gmail.com', 'sarvesh_13', 'abcd');
+values (34,'nikhil', 'abc', 'babar','27 oct','nikhilbabar@gmail.com', 'sarvesh_13', 'abcd');
 
 insert into Customer
-values (56,'Manan','Chajed','4 dec','Manan@gmail.com','manan_11', 'ijkl');
+values (50,'Sarvesh', 'def','Deshpande','19 sep','Sarvesh@gmail.com', 'sarvesh_13', 'abcd');
 
 insert into Customer
-values (35,'chaitya','dobariya','9 august','chaitya@gmail.com','chaitya_16', 'mnop');
+values (56,'Manan', 'ghi','Chajed','4 dec','Manan@gmail.com','manan_11', 'ijkl');
 
 insert into Customer
-values (10,'Tejas','durgudhe','10 oct','Tejas@gmail.com','tejas_16', 'qrst');
+values (35,'chaitya', 'jkl','dobariya','9 august','chaitya@gmail.com','chaitya_16', 'mnop');
+
+insert into Customer
+values (10,'Tejas', 'mno','durgudhe','10 oct','Tejas@gmail.com','tejas_16', 'qrst');
+
 
 insert into Contact_Customer
 values (948939299,34);
 
 insert into Contact_Customer
-values (849302584,34);
+values (849302584,50);
 
 insert into Contact_Customer
-values (948939299,50);
+values (948939299,56);
 
 insert into Contact_Customer
-values (849352584,50);
+values (849352584,35);
+
+insert into Contact_Customer
+values (844962584,10);
 
 
 insert into Account
@@ -102,6 +141,7 @@ values ('bijay_11', 'ijkl');
 insert into Account
 values ('jay_16', 'mnop');
     
+
 insert into Driver
 values (44,'ajay','kumar',10,4,"ajay@gmail.com",'ajay_13', 'abcd');
 
@@ -131,75 +171,3 @@ values (549352584,90);
 
 insert into Contact_Driver
 values (849352584,41);
-
-
-CREATE TABLE Routes(
-    Source varchar(30) NOT NULL,
-    Destination varchar(30) NOT NULL,
-    Distance int,
-    Fare int,
-    RouteNo int,
-    PRIMARY KEY (RouteNo)
-);
-
-CREATE TABLE Vehicle(
-    RegNo int NOT NULL,
-    Model varchar(20),
-    Type varchar(20),
-    LastService varchar(15),
-    PRIMARY KEY (RegNo)
-);
-
-CREATE TABLE Driver_Vehicle_relation(
-    DriverId int NOT NULL,
-    RegNo int NOT NULL,
-    FOREIGN KEY (DriverId) REFERENCES Driver(DriverId),
-    FOREIGN KEY (RegNo) REFERENCES Vehicle(RegNo),
-    PRIMARY KEY (DriverId, RegNo)
-);
-
-CREATE TABLE Journey(
-    CustomerId int,
-    DriverId int,
-    RouteNo int,
-    RegNo int,
-    FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
-    FOREIGN KEY (DriverId) REFERENCES Driver(DriverId),
-    FOREIGN KEY (RouteNo) REFERENCES Routes(RouteNo),
-    FOREIGN KEY (RegNo) REFERENCES Vehicle(RegNo),
-    PRIMARY KEY (CustomerId,DriverId, RouteNo, RegNo)    
-);
-
-INSERT INTO Routes 
-VALUES("Mumbai","Pune",152,3000,101);
-
-INSERT INTO Routes 
-VALUES("Thane","Andheri",22,1000,201);
-
-INSERT INTO Routes 
-VALUES("Andheri","Vasai",46,1200,301);
-
-INSERT INTO Vehicle 
-VALUES(11001,"Swift","Sedan","August");
-
-INSERT INTO Vehicle 
-VALUES(11002,"Ciaz","Sedan","September");
-
-INSERT INTO Vehicle 
-VALUES(11003,"Creta","SUV","July");
-
-INSERT INTO Vehicle 
-VALUES(11004,"Harrier","SUV","August");
-
-INSERT INTO Vehicle 
-VALUES(11005,"BMW X1","Luxury","October");
-
-INSERT INTO Driver_Vehicle_relation
-VALUES(11001,90);
-
-INSERT INTO Driver_Vehicle_relation
-VALUES(11002,34);
-
-INSERT INTO Driver_Vehicle_relation
-VALUES(11003,41);
-
